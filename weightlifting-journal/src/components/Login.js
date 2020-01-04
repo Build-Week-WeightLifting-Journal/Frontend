51ae36e3e67f94814e1cb6890c4964696da3f535
aaronjan <aaronjan98@gmail.com>
2020-01-04 14:12:56 -0800

    imported Calendar component into Login and Signup.js

---
+++ weightlifting-journal/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Calendar from "./Calendar";
+
+function Login() {
+    return null;
+}
+
+export default Login;
---
+++ weightlifting-journal/src/components/Signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Calendar from "./Calendar";
+
+function Signup() {
+    return null;
+}
+
+export default Signup;

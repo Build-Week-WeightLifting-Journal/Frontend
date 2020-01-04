e5da13cc16e3c9a07cc22c6ac434bc1c1e4c0ecc
aaronjan <aaronjan98@gmail.com>
2020-01-04 13:06:20 -0800

    added font-awesome

--- weightlifting-journal/src/App.js
+++ weightlifting-journal/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>Weight Lifting Journal</h1>
+      <button>Sign Up</button>
+      <button>Login</button>
     </div>
   );
 }

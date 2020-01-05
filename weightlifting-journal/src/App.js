e20660df887da1d1d1e46120ede9b9a861f6fc42
Kevin Schoell <schoell411@gmail.com>
2020-01-04 19:31:46 -0600

    calendar component built

--- weightlifting-journal/src/App.js
+++ weightlifting-journal/src/App.js
@@ -1,27 +1,27 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 import { Route, Link, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Calendar from "./components/Calendar";
 
 function App() {
   return (
     <div className="App">
-
       <nav>
-        <h1>Weight Lifting Journal</h1>
         <div>
+          <Link to="/">
+            <h1>Weight Lifting Journal</h1>
+          </Link>
           <Link to="/sign-up">Sign Up</Link>
           <Link to="/login">Login</Link>
+          <Link to="/calendar">Calendar</Link>
         </div>
       </nav>
 
       <Switch>
-        <Route exact path="/sign-up">
-          
-        </Route>
-        <Route path="/login">
-          
-        </Route>
+        <Route exact path="/sign-up"></Route>
+        <Route path="/login"></Route>
+        <Route path="/calendar" component={Calendar} />
       </Switch>
     </div>
   );

3644d0fd6d2b55453f5fd415b18444564b482fda
aaronjan <aaronjan98@gmail.com>
2020-01-07 16:06:01 -0800

    added different form style to WorkoutStyles.js

--- weightlifting-journal/src/components/dashboard/workout-list/WorkoutList.js
+++ weightlifting-journal/src/components/dashboard/workout-list/WorkoutList.js
@@ -1,99 +1,84 @@
-import React, { useReducer, useContext, useEffect, useRef } from 'react';
-import { WrapperDiv } from "./WorkoutStyles";
+import React, { useState} from 'react';
+import { WorkoutInfoDiv, WorkoutListDiv, WorkoutDiv, ButtonDiv} from "./WorkoutStyles";
 
-function appReducer(state, action) {
-  switch (action.type) {
-    case 'reset': {
-      return action.payload;
-    }
-    case 'add': {
-      return [
-        ...state,
-        {
-          id: Date.now(),
-          text: '',
-          completed: false,
-        },
-      ];
-    }
-    case 'delete': {
-      return state.filter(item => item.id !== action.payload);
-    }
-    case 'completed': {
-      return state.map(item => {
-        if (item.id === action.payload) {
-          return {
-            ...item,
-            completed: !item.completed,
-          };
-        }
-        return item;
-      });
-    }
-    default: {
-      return state;
-    }
-  }
-}
-
-const Context = React.createContext();
-
-function useEffectOnce(cb) {
-  const didRun = useRef(false);
-
-  useEffect(() => {
-    if (!didRun.current) {
-      cb();
-      didRun.current = true;
-    }
-  });
-}
-
-export default function TodosApp() {
-  const [state, dispatch] = useReducer(appReducer, []);
-
-  useEffectOnce(() => {
-    const raw = localStorage.getItem('data');
-    dispatch({ type: 'reset', payload: raw ? JSON.parse(raw) : [] });
-  });
-
-  useEffect(
-    () => {
-      localStorage.setItem('data', JSON.stringify(state));
-    },
-    [state]
-  );
-
-  return (
-    <Context.Provider value={dispatch}>
-      <h1>Workout List</h1><br />
-      <button onClick={() => dispatch({ type: 'add' })}>New Todo</button>
-      <br />
-      <br />
-      <TodosList items={state} />
-    </Context.Provider>
+const Workout = ({workout, index, completeWorkout, removeWorkout }) => {
+  return(
+    <WorkoutInfoDiv>
+        {workout.text}
+      <ButtonDiv>
+        <button onClick={() => completeWorkout(index)}>Complete</button>
+        <button onClick={() => removeWorkout(index)}>x</button>
+      </ButtonDiv>
+    </WorkoutInfoDiv>
   );
 }
 
-function TodosList({ items }) {
-  return items.map(item => <TodoItem key={item.id} {...item} />);
-}
+function WorkoutForm({ addWorkout }) {
+  const [value, setValue] = useState("");
 
-function TodoItem({ id, completed, text }) {
-  const dispatch = useContext(Context);
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addWorkout(value);
+    setValue("");
+  };
+
   return (
-    <WrapperDiv>
-    {/* <InputDiv> */}
+    <form onSubmit={handleSubmit}>
       <input
-        type="checkbox"
-        checked={completed}
-        onChange={() => dispatch({ type: 'completed', payload: id })}
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
       />
-    {/* </InputDiv> */}
-      <input type="text" defaultValue={text} />
-      <button onClick={() => dispatch({ type: 'delete', payload: id })}>
-        Delete
-      </button>
-    </WrapperDiv>
+    </form>
   );
 }
+
+const WorkoutList = () => {
+    const [workouts, setWorkouts] = useState([
+      {
+        text: "Test Workout #1",
+        isCompleted: false
+      },
+      {
+        text: "Test Workout #2",
+        isCompleted: false
+      },
+      {
+        text: "Test Workout #3",
+        isCompleted: false
+      }
+    ]);
+
+    const addWorkout = text => {
+      const newWorkouts = [...workouts, { text }];
+      setWorkouts(newWorkouts);
+    };
+  
+    const completeWorkout = index => {
+      const newWorkouts = [...workouts];
+      newWorkouts[index].isCompleted = true;
+      setWorkouts(newWorkouts);
+    };
+  
+    const removeWorkout = index => {
+      const newWorkouts = [...workouts];
+      newWorkouts.splice(index, 1);
+      setWorkouts(newWorkouts);
+    };
+
+
+    return(
+      <WorkoutListDiv>
+        <WorkoutDiv>
+            {workouts.map((workout, index) => (
+              <Workout key={index} index={index} workout={workout} completeWorkout={completeWorkout} removeWorkout={removeWorkout}/>
+            ))}
+            <WorkoutForm addWorkout={addWorkout} />
+        </WorkoutDiv>
+      </WorkoutListDiv>
+    );
+}
+
+export default WorkoutList;
--- weightlifting-journal/src/components/dashboard/workout-list/WorkoutStyles.js
+++ weightlifting-journal/src/components/dashboard/workout-list/WorkoutStyles.js
@@ -1,7 +1,48 @@
 import styled from "styled-components";
 
-export const WrapperDiv = styled.div`
+export const WorkoutListDiv = styled.div`
+    background: #fff;
+    padding: 10px;
+    width: 100vw;
+`;
+
+export const WorkoutInfoDiv = styled.div`
+    box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.15);
+    border-radius: 4px;
+    width: 100%;
     display: flex;
+    justify-content: space-between;
     align-items: center;
+    margin: 10px;
+
+`;
+
+export const WorkoutDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    background: #fff;
+    padding: 3px 0px;
+    font-size: 12px;
+    margin-bottom: 6px;
+    border-radius: 3px;
+
+`;
+
+export const ButtonDiv = styled.div`
+    border: none;
     margin: 0;
+    text-decoration: none;
+    cursor: pointer;
+`;
+
+export const InputDiv = styled.div`
+    box-sizing: border-box;
+    border: 0;
+    border-radius: 4px;
+    box-shadow: none;
+    padding: 1rem;
+    height: 2.25em;
+    
 `;

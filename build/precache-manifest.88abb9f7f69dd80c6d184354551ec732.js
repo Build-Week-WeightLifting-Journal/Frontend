b346e8c7b19fa1d14b1a64efd16ff74ebea0544b
aaronjan <aaronjan98@gmail.com>
2020-01-09 17:07:35 -0800

    hosting to heroku

---
+++ build/precache-manifest.88abb9f7f69dd80c6d184354551ec732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0696f262503d6b3a94e0ffc440f7fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a7438a0fce8d360b775",
+    "url": "/static/css/main.2d1eab3c.chunk.css"
+  },
+  {
+    "revision": "3902836d91c8c34a289f",
+    "url": "/static/js/2.3a9de24e.chunk.js"
+  },
+  {
+    "revision": "de1812ae279f88a061d6c018adf43301",
+    "url": "/static/js/2.3a9de24e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9a7438a0fce8d360b775",
+    "url": "/static/js/main.be199043.chunk.js"
+  },
+  {
+    "revision": "2efdf30afa9c2bf4034a",
+    "url": "/static/js/runtime-main.97dca8a7.js"
+  }
+]);
